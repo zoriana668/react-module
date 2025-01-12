bbff31593163157ef65fe2050a1b7f945f33cd5c
zoryana668 <zoryana668@gmail.com>
2025-01-12 21:24:04 +0200

    refactoring api service file and creating .env file

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import UsersComponent from "./components/users-component/UsersComponent.tsx";
 
 const App = () =>  {
 
-
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    //     .then(value => value.json())
-    //     .then(response => {
-    //         setUsers(response);
-    //     });
-
-
   return (
       <div>
           <UsersComponent/>
--- src/sevices/api.service.ts
+++ src/sevices/api.service.ts
@@ -1,15 +1,19 @@
 import {IUser} from "../models/IUser.ts";
 
-const baseUrl = 'https://jsonplaceholder.typicode.com/users';
+// const baseUrl = import.meta.env.VITE_API_URL;
 
-export const getUsers = async ():Promise<IUser[]> => {
-    const users = await fetch(baseUrl)
+const getUsers = async ():Promise<IUser[]> => {
+    return await fetch(import.meta.env.VITE_API_URL)
         .then(value => value.json());
-    return users;
 }
 
-export const getUser = async (id: string): Promise<IUser> => {
-    const user = await fetch(baseUrl + '/' + id)
+const getUser = async (id: string): Promise<IUser> => {
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then(value => value.json());
-    return user;
+}
+
+
+export {
+    getUsers,
+    getUser
 }
