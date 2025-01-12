83ec6c34ea42ac11e55a2adba20e55161cc15c39
zoryana668 <zoryana668@gmail.com>
2025-01-12 21:09:35 +0200

    refactoring api service file

--- src/sevices/api.service.ts
+++ src/sevices/api.service.ts
@@ -1,13 +1,15 @@
 import {IUser} from "../models/IUser.ts";
 
+const baseUrl = 'https://jsonplaceholder.typicode.com/users';
+
 export const getUsers = async ():Promise<IUser[]> => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch(baseUrl)
         .then(value => value.json());
     return users;
 }
 
 export const getUser = async (id: string): Promise<IUser> => {
-    const user = await fetch('https://jsonplaceholder.typicode.com/users' + id)
+    const user = await fetch(baseUrl + '/' + id)
         .then(value => value.json());
     return user;
 }
