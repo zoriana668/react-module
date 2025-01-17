56962c7d9655213241131f0771a2c4189dc8dd34
zoryana668 <zoryana668@gmail.com>
2025-01-17 21:41:59 +0200

    refctoring .env file link

--- src/sevices/api.service.ts
+++ src/sevices/api.service.ts
@@ -1,11 +1,11 @@
 import {ITodo} from "../models/ITodo.ts";
 
 export const getTodoLists = async ():Promise<ITodo> => {
-    return await fetch(import.meta.env.VITE_API_URL)
+    return await fetch(import.meta.env.VITE_API_URL_TODO)
         .then(value => value.json());
 }
 
 export const getTodoList = async (id: string): Promise<ITodo> => {
-    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
+    return await fetch(import.meta.env.VITE_API_URL_TODO + '/' + id)
         .then(value => value.json());
 }
